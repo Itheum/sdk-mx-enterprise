703b4cf93a8da751b68b78222ad5e27491e38990
Bucur David <dbucur22@gmail.com>
2023-09-26 17:30:19 +0300

    fix: contract on shard 1

--- src/config.ts
+++ src/config.ts
@@ -38,7 +38,7 @@ export const itheumTokenIdentifier: { [key in EnvironmentsEnum]: string } = {
 };
 
 export const factoryContractAddress: { [key in EnvironmentsEnum]: string } = {
-  devnet: 'erd1qqqqqqqqqqqqqpgqfgj4yax79ssekq0xdpw7cxt7xrcj8ynpw3wqnadraf',
+  devnet: 'erd1qqqqqqqqqqqqqpgqfamd0z208q0clr9n4nxdat5ufu8p73an8yps9xzg8n',
   devnet2: '',
   mainnet: '',
   testnet: ''
